3eff8410a8e55c5929a997560a1da30ab5f0a41f
Dermoclinic <appdermoclinic@gmail.com>
2025-12-24 12:01:56 -0500

    Sellfy V 2.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -8,8 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
   return {
     plugins: [react()],
     resolve: {
